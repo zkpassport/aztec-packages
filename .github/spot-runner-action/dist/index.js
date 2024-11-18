61042698d80859c8c93df71133bbf6b66ff98071
ludamad <adam.domurad@gmail.com>
2024-11-19 00:03:34 +0700

    fix: don't take down runners with faulty runner check (#10019)
    Things work technically but this will cause interference unless merged
    
    ---------
    
    Co-authored-by: Charlie Lye <karl.lye@gmail.com>

--- .github/spot-runner-action/dist/index.js
+++ .github/spot-runner-action/dist/index.js
@@ -699,10 +699,11 @@ function pollSpotStatus(config, ec2Client, ghClient) {
             }
             try {
                 core.info("Found ec2 instance, looking for runners.");
-                if (process.env.WAIT_FOR_RUNNERS === "false" || (yield ghClient.hasRunner([config.githubJobId]))) {
-                    // we have runners
-                    return instances[0].InstanceId;
-                }
+                // TODO find out whatever happened here but we seem to not be able to wait for runners
+                //if (process.env.WAIT_FOR_RUNNERS === "false" || await ghClient.hasRunner([config.githubJobId])) {
+                // we have runners
+                return instances[0].InstanceId;
+                //}
             }
             catch (err) { }
             // wait 10 seconds
